5f5af9b12bf0e1dc4457b19fd2ff55411d9adf44
Moinkey2 <83717905+Moinkey2@users.noreply.github.com>
2021-05-05 13:47:33 -0500

    Update IdentifierShuffler.java

--- src/main/java/com/uniquepassive/mystery/core/obfuscators/shuffling/IdentifierShuffler.java
+++ src/main/java/com/uniquepassive/mystery/core/obfuscators/shuffling/IdentifierShuffler.java
@@ -9,19 +9,22 @@ public class IdentifierShuffler {
     public void run(Map<String, ClassNode> targetClasses) {
         targetClasses.forEach((name, c) -> {
             Set<List<?>> targets = new HashSet<>();
-
+            
+            // all
             targets.addAll(Arrays.asList(
                     c.fields, c.methods,
                     c.attrs, c.innerClasses,
                     c.visibleAnnotations, c.invisibleAnnotations,
                     c.visibleTypeAnnotations, c.invisibleTypeAnnotations)
             );
-
+            
+            // fields
             c.fields.forEach(f -> targets.addAll(Arrays.asList(
                     f.visibleAnnotations, f.invisibleAnnotations,
                     f.visibleTypeAnnotations, f.invisibleTypeAnnotations)
             ));
-
+            
+            // methods
             c.methods.forEach(m -> targets.addAll(Arrays.asList(
                     m.exceptions, m.localVariables,
                     m.visibleAnnotations, m.invisibleAnnotations,
